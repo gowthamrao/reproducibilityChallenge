e0f61ff6bd76d45fff69e8e12ec67c3155808de4
Gowtham Rao <grao9@its.jnj.com>
2021-09-13 14:05:42 +0000

    Initial commit

---
+++ facultyCohortDiagnostics/extras/examplesOfCodeToRun/exampleRedShiftSerial.R
@@ -0,0 +1,145 @@
+############### Note this is a custom script, a version of CodeToRun.R that may not work for everyone ##############
+############### Please use CodeToRun.R as it a more generic version  ###############################################
+
+############### This version of code will loop over each database ids in serial                      ###############
+
+############### processes to run Cohort Diagnostics ################################################################
+library(magrittr)
+
+################################################################################
+# VARIABLES - please change
+################################################################################
+# The folder where the study intermediate and result files will be written:
+outputFolder <- "D:/studyResults/OHDSISymposium2021ReproducibilityFaculty"
+# create output directory if it does not exist
+if (!dir.exists(outputFolder)) {
+  dir.create(outputFolder,
+             showWarnings = FALSE,
+             recursive = TRUE)
+}
+# Optional: specify a location on your disk drive that has sufficient space.
+# options(andromedaTempFolder = "s:/andromedaTemp")
+
+# set to false if email is not possible
+mailFatal <- FALSE
+
+# do you want to upload the results to a local database
+uploadToLocalPostGresDatabase <- FALSE
+
+############## databaseIds to run cohort diagnostics on that source  #################
+databaseIds <-
+  c(
+    'truven_ccae',
+    'truven_mdcd',
+    'truven_mdcr',
+    'cprd',
+    'jmdc',
+    'optum_extended_dod',
+    'optum_ehr',
+    'ims_australia_lpd',
+    'ims_germany',
+    'ims_france'
+  )
+
+## service name for keyring for db with cdm
+keyringUserService <- 'OHDSI_USER'
+keyringPasswordService <- 'OHDSI_PASSWORD'
+
+## service name for keyring for postgres db to upload results
+keyringUserServicePostGresUpload <- 'shinydbUser'
+keyringPasswordServicePostGresUpload <- 'shinydbPW'
+keyringDatabaseServicePostGresUpload <- 'shinydbDatabase'
+keyringServerServicePostGresUpload <- 'shinydbServer'
+keyringPortServicePostGresUpload <- 'shinydbPort'
+
+# lets get meta information for each of these databaseId. This includes connection information.
+source("extras/examplesOfCodeToRun/dataSourceInformation.R")
+cdmSources <- cdmSources %>% 
+  dplyr::filter(.data$version == 1676)
+
+## if uploading to co-ordinator site
+privateKeyFileName <- ""
+siteUserName <- ""
+
+###### create a list object that contain connection and meta information for each data source
+x <- list()
+for (i in (1:length(databaseIds))) {
+  databaseId <- databaseIds[[i]]
+  cdmSource <- cdmSources %>%
+    dplyr::filter(.data$sequence == 1) %>% 
+    dplyr::filter(database == databaseId)
+  
+  if (uploadToLocalPostGresDatabase) {
+    uploadToLocalPostGresDatabaseSpecifications <- list(
+      connectionDetails = DatabaseConnector::createConnectionDetails(
+        dbms = "postgresql",
+        server = paste(
+          keyring::key_get(keyringServerServicePostGresUpload),
+          keyring::key_get(keyringDatabaseServicePostGresUpload),
+          sep = "/"
+        ),
+        port = keyring::key_get(keyringPortServicePostGresUpload),
+        user = keyring::key_get(keyringUserServicePostGresUpload),
+        password = keyring::key_get(keyringPasswordServicePostGresUpload)
+      ),
+      schema = 'OHDSISymposium2021ReproducibilityFaculty',
+      zipFileName = list.files(
+        path = file.path(outputFolder, databaseId),
+        pattern = paste0("Results_", databaseId, ".zip"),
+        full.names = TRUE,
+        recursive = TRUE
+      )
+    )
+  } else {
+    uploadToLocalPostGresDatabaseSpecifications <- ''
+  }
+  
+  x[[i]] <- list(
+    cdmSource = cdmSource,
+    generateCohortTableName = TRUE,
+    verifyDependencies = TRUE,
+    databaseId = databaseId,
+    outputFolder = file.path(outputFolder, databaseId),
+    userService = keyringUserService,
+    passwordService = keyringPasswordService,
+    preMergeDiagnosticsFiles = TRUE,
+    privateKeyFileName = privateKeyFileName,
+    userName = siteUserName,
+    uploadToLocalPostGresDatabaseSpecifications = uploadToLocalPostGresDatabaseSpecifications
+  )
+}
+
+
+############ executeOnMultipleDataSources #################
+# x <- x[1:2]
+
+for (i in (1:length(x))) {
+  executeOnMultipleDataSources(x[[i]])
+}
+# 
+# # launch cohort explorer
+# for (i in (1:length(x))) {
+#   cohortTableName <- paste0(
+#     stringr::str_squish(x$databaseId),
+#     stringr::str_squish("OHDSISymposium2021ReproducibilityFaculty")
+#   )
+#   # Details for connecting to the server:
+#   connectionDetails <-
+#     DatabaseConnector::createConnectionDetails(
+#       dbms = x$cdmSource$dbms,
+#       server = x$cdmSource$server,
+#       user = keyring::key_get(service = x$userService),
+#       password =  keyring::key_get(service = x$passwordService),
+#       port = x$cdmSource$port
+#     )
+#   cdmDatabaseSchema <- x$cdmSource$cdmDatabaseSchema
+#   cohortDatabaseSchema <- x$cdmSource$cohortDatabaseSchema
+#   CohortDiagnostics::launchCohortExplorer(connectionDetails = connectionDetails,
+#                                           cdmDatabaseSchema = cdmDatabaseSchema,
+#                                           cohortDatabaseSchema = cohortDatabaseSchema,
+#                                           cohortTable = cohortTable, 
+#                                           cohortId = -1
+#   )
+# }
+# 
+# 
---
+++ studentCohortDiagnostics/extras/examplesOfCodeToRun/dataSourceInformation.R
@@ -0,0 +1,191 @@
+source(Sys.getenv("startUpScriptLocation"))
+
+######
+executeOnMultipleDataSources <- function(x) {
+  if (x$generateCohortTableName) {
+    cohortTableName <- paste0(
+      stringr::str_squish(x$databaseId),
+      stringr::str_squish("OHDSISymposium2021Reproducibility")
+    )
+  }
+  
+  # this function gets details of the data source from cdm source table in omop, if populated.
+  # The assumption is the cdm_source.sourceDescription has text description of data source.
+  getDataSourceDetails <-
+    function(connection,
+             databaseId,
+             cdmDatabaseSchema) {
+      sqlCdmDataSource <- "select * from @cdmDatabaseSchema.cdm_source;"
+      sourceInfo <- list(cdmSourceName = databaseId,
+                         sourceDescription = databaseId)
+      tryCatch(
+        expr = {
+          cdmDataSource <-
+            DatabaseConnector::renderTranslateQuerySql(
+              connection = connection,
+              sql = sqlCdmDataSource,
+              cdmDatabaseSchema = cdmDatabaseSchema,
+              snakeCaseToCamelCase = TRUE
+            ) %>% dplyr::tibble()
+          if (nrow(cdmDataSource) == 0) {
+            return(sourceInfo)
+          }
+          sourceInfo$sourceDescription <- ""
+          if ("sourceDescription" %in% colnames(cdmDataSource)) {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " ",
+                                                   cdmDataSource$sourceDescription) %>% 
+              stringr::str_trim()
+          }
+          if ("cdmSourceName" %in% colnames(cdmDataSource)) {
+            sourceInfo$cdmSourceName <- cdmDataSource$cdmSourceName
+          }
+          if ("cdmEtlReference" %in% colnames(cdmDataSource)) {
+            if (length(cdmDataSource$cdmEtlReference) > 4) {
+              sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                     " ETL Reference: ",
+                                                     cdmDataSource$cdmEtlReference) %>% 
+                stringr::str_trim()
+            } else {
+              sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                     " ETL Reference: None") %>% 
+                stringr::str_trim()
+            }
+          }
+          if ("sourceReleaseDate" %in% colnames(cdmDataSource)) {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " CDM release date: ",
+                                                   as.character(cdmDataSource$sourceReleaseDate)) %>% 
+              stringr::str_trim()
+          } else {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " CDM release date: None") %>% 
+              stringr::str_trim()
+          }
+          if ("sourceReleaseDate" %in% colnames(cdmDataSource)) {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " Source release date: ",
+                                                   as.character(cdmDataSource$sourceReleaseDate)) %>% 
+              stringr::str_trim()
+          } else {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " Source release date: None") %>% 
+              stringr::str_trim()
+          }
+          if ("sourceDocumentationReference" %in% colnames(cdmDataSource)) {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " Source Documentation Reference: ",
+                                                   as.character(cdmDataSource$sourceDocumentationReference)) %>% 
+              stringr::str_trim()
+          } else {
+            sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                   " Source Documentation Reference: None") %>% 
+              stringr::str_trim()
+          }
+        },
+        error = function(...) {
+          return(sourceInfo)
+        }
+      )
+      return(sourceInfo)
+    }
+  
+  # Details for connecting to the server:
+  connectionDetails <-
+    DatabaseConnector::createConnectionDetails(
+      dbms = x$cdmSource$dbms,
+      server = x$cdmSource$server,
+      user = keyring::key_get(service = x$userService),
+      password =  keyring::key_get(service = x$passwordService),
+      port = x$cdmSource$port
+    )
+  # The name of the database schema where the CDM data can be found:
+  cdmDatabaseSchema <- x$cdmSource$cdmDatabaseSchema
+  cohortDatabaseSchema <- x$cdmSource$cohortDatabaseSchema
+  
+  connection <-
+    DatabaseConnector::connect(connectionDetails = connectionDetails)
+  dataSourceDetails <- getDataSourceDetails(
+    connection = connection,
+    databaseId = x$databaseId,
+    cdmDatabaseSchema = cdmDatabaseSchema
+  )
+  DatabaseConnector::disconnect(connection)
+  
+  OHDSISymposium2021Reproducibility::execute(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTableName,
+    verifyDependencies = x$verifyDependencies,
+    outputFolder = x$outputFolder,
+    databaseId = x$databaseId,
+    databaseName = dataSourceDetails$cdmSourceName,
+    databaseDescription = dataSourceDetails$sourceDescription
+  )
+  
+  if (x$preMergeDiagnosticsFiles) {
+    CohortDiagnostics::preMergeDiagnosticsFiles(dataFolder = x$outputFolder)
+  }
+  
+  if (length(x$privateKeyFileName) > 0 &&
+      !x$privateKeyFileName == "" &&
+      length(x$userName) > 0 &&
+      !x$userName == "") {
+    CohortDiagnostics::uploadResults(x$outputFolder,
+                                     x$privateKeyFileName,
+                                     x$userName)
+  }
+  
+  if (length(x$uploadToLocalPostGresDatabaseSpecifications) > 1) {
+    # Set the POSTGRES_PATH environmental variable to the path to the folder containing the psql executable to enable bulk upload (recommended).
+    
+    connectionPostGres <-
+      DatabaseConnector::connect(x$uploadToLocalPostGresDatabaseSpecifications$connectionDetails)
+    
+    # check if schema was instantiated
+    sqlSchemaCheck <-
+      paste0(
+        "SELECT * FROM information_schema.schemata WHERE schema_name = '",
+        tolower(x$uploadToLocalPostGresDatabaseSpecifications$schema),
+        "';"
+      )
+    schemaExists <-
+      DatabaseConnector::renderTranslateQuerySql(connection = connectionPostGres,
+                                                 sql = sqlSchemaCheck)
+    
+    if (nrow(schemaExists) == 0) {
+      warning(
+        paste0(
+          "While attempting to upload to postgres, found target schema to not exist - attempting to create target schema ",
+          x$uploadToLocalPostGresDatabaseSpecifications$schema
+        )
+      )
+      createSchemaSql <-
+        paste0(
+          "select create_schema('",
+          tolower(x$uploadToLocalPostGresDatabaseSpecifications$schema),
+          ");"
+        )
+      DatabaseConnector::renderTranslateQuerySql(connection = connectionPostGres,
+                                                 sql = createSchemaSql)
+      ParallelLogger::logInfo("Schema created.")
+      
+    }
+    # check if required table exists, else create them
+    if (!DatabaseConnector::dbExistsTable(conn = connectionPostGres, name = "cohort_count")) {
+      CohortDiagnostics::createResultsDataModel(
+        connection = connectionPostGres,
+        schema = tolower(x$uploadToLocalPostGresDatabaseSpecifications$schema)
+      )
+    }
+    
+    DatabaseConnector::disconnect(connection = connectionPostGres)
+    # note this is a thread safe upload, so its ok to parallelize
+    CohortDiagnostics::uploadResults(
+      connectionDetails = x$uploadToLocalPostGresDatabaseSpecifications$connectionDetails,
+      schema = x$uploadToLocalPostGresDatabaseSpecifications$schema,
+      zipFileName = x$uploadToLocalPostGresDatabaseSpecifications$zipFileName
+    )
+  }
+}
